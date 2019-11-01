28a4e86125c49a4d05bcb2d111b99e07ec756482
Max Rietmann <mrietmann@nvidia.com>
2019-11-01 16:55:46 +0100

    WIP Commit: Plotting and Newton comparison.

--- cpp/src_prims/optimization/lbfgs_b.h
+++ cpp/src_prims/optimization/lbfgs_b.h
@@ -60,6 +60,19 @@ std::string LBFGSB_RESULT_STRING(LBFGSB_RESULT res) {
 
 class Batched_LBFGS_B {
  public:
+  void minimizeNewton(std::function<void(const std::vector<Eigen::VectorXd>& x,
+                                         std::vector<double>& fx)>
+                        f,
+                      std::function<void(const std::vector<Eigen::VectorXd>& x,
+                                         std::vector<Eigen::VectorXd>& gfx)>
+                        g,
+                      std::function<void(const std::vector<Eigen::VectorXd>& x,
+                                         std::vector<Eigen::MatrixXd>& hfx)>
+                        hess,
+                      std::vector<Eigen::VectorXd>& x,
+                      std::vector<LBFGSB_RESULT>& status, std::string& info_str,
+                      std::vector<std::vector<Eigen::VectorXd>>& xk_all);
+
   void minimize(std::function<void(const std::vector<Eigen::VectorXd>& x,
                                    std::vector<double>& fx)>
                   f,
@@ -69,12 +82,17 @@ class Batched_LBFGS_B {
                 const std::vector<double>& fx0,
                 const std::vector<Eigen::VectorXd>& gx0,
                 std::vector<Eigen::VectorXd>& x,
-                std::vector<LBFGSB_RESULT>& status, std::string& info_str);
+                std::vector<LBFGSB_RESULT>& status, std::string& info_str,
+                std::vector<std::vector<Eigen::VectorXd>>& xk_all);
 
   Batched_LBFGS_B(int verbosity = -1, int maxiter = 1000, int M = 10,
                   double pgtol = 1e-5, double factr = 1e7, int maxls = 20);
 
  private:
+  void compute_pk_single_bfgs(const Eigen::VectorXd& gk,
+                              const Eigen::VectorXd& sk,
+                              const Eigen::VectorXd& yk, Eigen::MatrixXd& Hk,
+                              Eigen::Ref<Eigen::VectorXd> pk);
   void compute_pk_single(const Eigen::VectorXd& gk,
                          const std::deque<Eigen::VectorXd>& sk,
                          const std::deque<Eigen::VectorXd>& yk,
@@ -107,6 +125,118 @@ Batched_LBFGS_B::Batched_LBFGS_B(int verbosity, int maxiter, int M,
     m_factr(factr),
     m_maxls(maxls) {}
 
+void Batched_LBFGS_B::minimizeNewton(
+  std::function<void(const std::vector<Eigen::VectorXd>&, std::vector<double>&)>
+    f,
+  std::function<void(const std::vector<Eigen::VectorXd>&,
+                     std::vector<Eigen::VectorXd>&)>
+    g,
+  std::function<void(const std::vector<Eigen::VectorXd>&,
+                     std::vector<Eigen::MatrixXd>&)>
+    hess,
+  std::vector<Eigen::VectorXd>& x, std::vector<LBFGSB_RESULT>& status,
+  std::string& info_str, std::vector<std::vector<Eigen::VectorXd>>& xk_all) {
+  const int batchSize = x.size();
+  const int N = x[0].size();
+  status.resize(batchSize);
+  xk_all.push_back(x);
+  std::vector<Eigen::VectorXd> xk(batchSize);
+  std::vector<Eigen::VectorXd> xkp1(batchSize);
+  std::vector<Eigen::VectorXd> pk(batchSize);
+  std::vector<Eigen::VectorXd> gk(batchSize);
+  std::vector<Eigen::MatrixXd> hk(batchSize);
+  std::vector<double> alpha(batchSize);
+  std::vector<double> fk(batchSize);
+  std::vector<double> fkp1(batchSize);
+  for (int i = 0; i < batchSize; i++) {
+    status[i] = LBFGSB_INCOMPLETE;
+    gk[i].resize(N);
+    xk[i] = x[i];
+  }
+  for (int k = 0; k < m_maxiter; k++) {
+    g(xk, gk);
+    hess(xk, hk);
+
+    // compute search direction
+    for (int ib = 0; ib < batchSize; ib++) {
+      pk[ib] = -hk[ib].colPivHouseholderQr().solve(gk[ib]);
+      alpha[ib] = 1.0;
+    }
+
+    std::vector<bool> ls_success(batchSize, false);
+    for (int ils = 0; ils < m_maxls; ils++) {
+      f(xk, fk);
+      for (int ib = 0; ib < batchSize; ib++) {
+        if (ls_success[ib]) continue;
+        xkp1[ib] = xk[ib] + alpha[ib] * pk[ib];
+      }
+      f(xkp1, fkp1);
+      for (int ib = 0; ib < batchSize; ib++) {
+        if (ls_success[ib]) continue;
+        if (fkp1[ib] < fk[ib]) {
+          ls_success[ib] = true;
+          if (m_verbosity > 0 && k % m_verbosity == 0) {
+            printf("k=%d, bid=%d: line search iterations=%d\n", k, ib, ils);
+          }
+          if (m_verbosity >= 100)
+            printf("k=%d, bid=%d: successful alpha=%f\n", k, ib, alpha[ib]);
+        } else {
+          if (m_verbosity >= 100)
+            printf("k=%d, bid=%d: unsuccessful alpha=%f\n", k, ib, alpha[ib]);
+        }
+        alpha[ib] *= 0.5;  // shrink stepsized by half;
+      }
+      // if all true, stop line search
+      if (std::all_of(ls_success.begin(), ls_success.end(),
+                      [](bool v) { return v; }))
+        break;
+
+      // if we needed all line-search iterations, return with error.
+      if (ils == m_maxls - 1) {
+        for (int ib = 0; ib < batchSize; ib++) {
+          if (ls_success[ib] == false) status[ib] = LBFGSB_STOP_MAXLS;
+          x = xk;
+          return;
+        }
+      }
+    }
+
+    // take step and update LBFGS-sk variable
+    for (int ib = 0; ib < batchSize; ib++) {
+      xkp1[ib] = xk[ib] + alpha[ib] * pk[ib];
+    }
+
+    // update gradient and LBFGS-gk variables
+    g(xkp1, gk);
+
+    // stopping criterion
+    f(xk, fk);
+    f(xkp1, fkp1);
+    for (int ib = 0; ib < batchSize; ib++) {
+      auto g_norm = gk[ib].norm();
+      if (k % m_verbosity == 0 || m_verbosity >= 100) {
+        printf("k=%d, bid=%d: |f|=%e -> %e, |g|=%e\n", k, ib, std::abs(fk[ib]),
+               std::abs(fkp1[ib]), g_norm);
+      }
+      if (g_norm < m_pgtol) {
+        status[ib] = LBFGSB_STOP_GTOL;
+      }
+      if (status[ib] == LBFGSB_INCOMPLETE && k == m_maxiter - 1) {
+        status[ib] = LBFGSB_STOP_ITER;
+      }
+    }
+    bool stop = true;
+    for (int ib = 0; ib < batchSize; ib++) {
+      if (status[ib] == LBFGSB_INCOMPLETE) {
+        stop = false;
+      }
+    }
+    if (stop) break;
+    xk = xkp1;
+    xk_all.push_back(xk);
+  }
+}
+
 void Batched_LBFGS_B::minimize(
   std::function<void(const std::vector<Eigen::VectorXd>& x,
                      std::vector<double>& fx)>
@@ -116,13 +246,15 @@ void Batched_LBFGS_B::minimize(
     g,
   const std::vector<double>& fx0, const std::vector<Eigen::VectorXd>& gx0,
   std::vector<Eigen::VectorXd>& x, std::vector<LBFGSB_RESULT>& status,
-  std::string& info_str) {
+  std::string& info_str, std::vector<std::vector<Eigen::VectorXd>>& xk_all) {
   const int batchSize = x.size();
   const int N = x[0].size();
   status.resize(batchSize);
 
   m_M_sk.resize(batchSize);
   m_M_yk.resize(batchSize);
+
+  xk_all.push_back(x);
 
   std::vector<Eigen::VectorXd> xk(batchSize);
   std::vector<Eigen::VectorXd> xkp1(batchSize);
@@ -132,11 +264,14 @@ void Batched_LBFGS_B::minimize(
   std::vector<double> fkp1(batchSize);
   std::vector<Eigen::VectorXd> gk(batchSize);
   std::vector<Eigen::VectorXd> gkp1(batchSize);
+  std::vector<Eigen::MatrixXd> Hk(batchSize);
   for (int i = 0; i < batchSize; i++) {
     status[i] = LBFGSB_INCOMPLETE;
     gk[i].resize(N);
     xk[i] = x[i];
+    Hk[i] = Eigen::MatrixXd::Identity(N, N);
   }
+
   for (int k = 0; k < m_maxiter; k++) {
     for (int ib = 0; ib < batchSize; ib++) {
       // compute search direction pk
@@ -172,9 +307,14 @@ void Batched_LBFGS_B::minimize(
         if (ls_success[ib]) continue;
         if (fkp1[ib] < fk[ib]) {
           ls_success[ib] = true;
-          printf("k=%d, bid=%d: successful alpha=%f\n", k, ib, alpha[ib]);
+          if (m_verbosity > 0 && k % m_verbosity == 0) {
+            printf("k=%d, bid=%d: line search iterations=%d\n", k, ib, ils);
+          }
+          if (m_verbosity >= 100)
+            printf("k=%d, bid=%d: successful alpha=%f\n", k, ib, alpha[ib]);
         } else {
-          printf("k=%d, bid=%d: unsuccessful alpha=%f\n", k, ib, alpha[ib]);
+          if (m_verbosity >= 100)
+            printf("k=%d, bid=%d: unsuccessful alpha=%f\n", k, ib, alpha[ib]);
         }
         alpha[ib] *= 0.5;  // shrink stepsized by half;
       }
@@ -232,7 +372,9 @@ void Batched_LBFGS_B::minimize(
     }
     if (stop) break;
     xk = xkp1;
+    xk_all.push_back(xk);
   }  // for k in maxiter
+
   x = xk;
 }
 
@@ -258,6 +400,25 @@ void Batched_LBFGS_B::compute_pk_single(const Eigen::VectorXd& gk,
     r = r + sk[i] * (alpha(i) - beta);
   }
   pk = -r;
+  // pk = -gk;  // use SD for now...
+}
+
+/**
+   Implements Alg. 7.4 from "Numerical Optimization"
+*/
+void Batched_LBFGS_B::compute_pk_single_bfgs(const Eigen::VectorXd& gk,
+                                             const Eigen::VectorXd& sk,
+                                             const Eigen::VectorXd& yk,
+                                             Eigen::MatrixXd& Hk,
+                                             Eigen::Ref<Eigen::VectorXd> pk) {
+  using Eigen::MatrixXd;
+  int N = Hk.rows();
+  double rhok = 1 / (yk.dot(sk));
+  MatrixXd Hkp1 = (MatrixXd::Identity(N, N) - rhok * sk * yk.transpose()) * Hk *
+                    (MatrixXd::Identity(N, N) - rhok * yk * sk.transpose()) +
+                  rhok * sk * sk.transpose();
+  pk = -Hkp1 * gk;  // use SD for now...
+  Hk = Hkp1;
 }
 
 }  // namespace Optimization
--- cpp/test/prims/lbfgsb.cpp
+++ cpp/test/prims/lbfgsb.cpp
@@ -26,6 +26,9 @@
 // #include "random/rng.h"
 // #include "test_utils.h"
 
+#include "matplotlibcpp.h"
+namespace plt = matplotlibcpp;
+
 namespace MLCommon {
 namespace Optimization {
 
@@ -45,8 +48,19 @@ void g_rosenbrock(const Eigen::VectorXd& x, Eigen::Ref<Eigen::VectorXd> g,
                   double a = 1.0, double b = 100.0) {
   // g = np.array([-2*a - 4*b*x[0]*(-x[0]**2 + x[1]) + 2*x[0],
   // b*(-2*x[0]**2 + 2*x[1])])
-  g[0] = -2 * a - 4 * b * x[0] * (pow(-x[0], 2) + x[1]) + 2 * x[0];
-  g[1] = -2 * pow(x[0], 2) + 2 * x[1];
+  g[0] = -2.0 * a - 4 * b * x[0] * (-pow(x[0], 2.0) + x[1]) + 2.0 * x[0];
+  g[1] = b * (-2.0 * pow(x[0], 2.0) + 2.0 * x[1]);
+}
+
+void h_rosenbrock(const Eigen::VectorXd& x, Eigen::Ref<Eigen::MatrixXd> H,
+                  double a = 1.0, double b = 100.0) {
+  // g = np.array([-2*a - 4*b*x[0]*(-x[0]**2 + x[1]) + 2*x[0],
+  // b*(-2*x[0]**2 + 2*x[1])])
+
+  H(0, 0) = 8 * b * pow(x[0], 2) - 4 * b * (-pow(x[0], 2) + x[1]) + 2;
+  H(0, 1) = -4 * b * x[0];
+  H(1, 0) = -4 * b * x[0];
+  H(1, 1) = 2 * b;
 }
 
 void batched_rosenbrock(const vector<Eigen::VectorXd>& x, int batchSize,
@@ -67,6 +81,15 @@ void batched_g_rosenbrock(const vector<Eigen::VectorXd>& x, int batchSize,
     g_rosenbrock(x[i], gx[i], a[i], b[i]);
   }
 }
+void batched_h_rosenbrock(const vector<Eigen::VectorXd>& x, int batchSize,
+                          const vector<double>& a, const vector<double>& b,
+                          vector<Eigen::MatrixXd>& Hx) {
+  Hx.resize(batchSize);
+  for (int i = 0; i < batchSize; i++) {
+    Hx[i].resize(2, 2);
+    h_rosenbrock(x[i], Hx[i], a[i], b[i]);
+  }
+}
 
 template <typename T>
 class LBFGSBTest : public ::testing::TestWithParam<LBFGSBInputs<T>> {
@@ -80,9 +103,10 @@ class LBFGSBTest : public ::testing::TestWithParam<LBFGSBInputs<T>> {
     vector<double> br(batchSize);
 
     std::random_device rd{};
-    std::mt19937 gen(rd());
-    std::normal_distribution<> da{1, 0.1};
-    std::normal_distribution<> db{100, 10};
+    // std::mt19937 gen(rd());
+    std::mt19937 gen(42);
+    std::normal_distribution<> da{1, 0.01};
+    std::normal_distribution<> db{100, 1};
     for (int i = 0; i < batchSize; i++) {
       ar[i] = da(gen);
       br[i] = db(gen);
@@ -95,19 +119,67 @@ class LBFGSBTest : public ::testing::TestWithParam<LBFGSBInputs<T>> {
                   vector<Eigen::VectorXd>& gfx) {
       batched_g_rosenbrock(x, batchSize, ar, br, gfx);
     };
+    auto hf = [&](const vector<Eigen::VectorXd>& x,
+                  vector<Eigen::MatrixXd>& hfx) {
+      batched_h_rosenbrock(x, batchSize, ar, br, hfx);
+    };
 
-    Batched_LBFGS_B opt(1, 10);
+    Batched_LBFGS_B opt(1, 100);
     std::vector<LBFGSB_RESULT> status;
     std::vector<Eigen::VectorXd> x0(batchSize);
     for (int ib = 0; ib < batchSize; ib++) {
-      x0[ib] = Eigen::VectorXd::Zero(2);
+      x0[ib].resize(2);
+      x0[ib][0] = ar[ib] + 0.1;
+      x0[ib][1] = ar[ib] * ar[ib] + 0.1;
     }
+
+    // test gradient
+    vector<Eigen::VectorXd> grad_fd(batchSize);
+    vector<Eigen::VectorXd> grad_test(batchSize);
+    double h = 1e-12;
+    for (int ib = 0; ib < batchSize; ib++) {
+      grad_fd[ib] = Eigen::VectorXd::Zero(2);
+      for (int i = 0; i < 2; i++) {
+        auto x0_0 = x0[ib][i];
+
+        vector<double> gfx_ph;
+        vector<double> gfx_mh;
+
+        // +h
+        x0[ib][i] = x0_0 + h;
+        f(x0, gfx_ph);
+
+        // -h
+        x0[ib][i] = x0_0 - h;
+        f(x0, gfx_mh);
+
+        // restore x0
+        x0[ib][i] = x0_0;
+
+        // second order
+        grad_fd[ib][i] = (gfx_ph[ib] - gfx_mh[ib]) / (2 * h);
+      }
+    }
+
+    gf(x0, grad_test);
+    printf("g=[\n");
+    for (int ib = 0; ib < batchSize; ib++) {
+      for (int i = 0; i < 2; i++) {
+        printf("fd:%f vs an:%f,\n", grad_fd[ib][i], grad_test[ib][i]);
+      }
+    }
+    printf("]\n");
+
     std::string info_str;
     std::vector<double> fx0;
     f(x0, fx0);
     std::vector<Eigen::VectorXd> gx0;
     gf(x0, gx0);
-    opt.minimize(f, gf, fx0, gx0, x0, status, info_str);
+    std::vector<std::vector<Eigen::VectorXd>> xk_all;
+    std::vector<std::vector<Eigen::VectorXd>> xk_Newton_all;
+    std::vector<Eigen::VectorXd> x0H = x0;
+    opt.minimize(f, gf, fx0, gx0, x0, status, info_str, xk_all);
+    opt.minimizeNewton(f, gf, hf, x0H, status, info_str, xk_Newton_all);
     for (int ib = 0; ib < batchSize; ib++) {
       if (status[ib] != LBFGSB_STOP_GTOL) {
         printf(
@@ -117,6 +189,39 @@ class LBFGSBTest : public ::testing::TestWithParam<LBFGSBInputs<T>> {
       }
       printf("Res[%d]=(%e,%e)\n", ib, x0[ib][0], x0[ib][1]);
     }
+
+    auto x_to_xy = [](std::vector<std::vector<Eigen::VectorXd>>& xin, int bid,
+                      std::vector<double>& x, std::vector<double>& y) {
+      // auto n_dof = xin[0][0].size() / 2;
+      auto n_elem = xin.size();
+      x.resize(n_elem);
+      y.resize(n_elem);
+      for (int i = 0; i < n_elem; i++) {
+        x[i] = xin[i][bid][0];
+        y[i] = xin[i][bid][1];
+      }
+    };
+
+    // plot results
+    plt::subplot(2, 1, 1);
+    std::vector<double> x0t;
+    std::vector<double> y0t;
+    std::vector<double> x1t;
+    std::vector<double> y1t;
+    std::vector<double> x0Ht;
+    std::vector<double> y0Ht;
+    std::vector<double> x1Ht;
+    std::vector<double> y1Ht;
+    x_to_xy(xk_all, 0, x0t, y0t);
+    x_to_xy(xk_all, 1, x1t, y1t);
+    x_to_xy(xk_Newton_all, 0, x0Ht, y0Ht);
+    x_to_xy(xk_Newton_all, 1, x1Ht, y1Ht);
+    plt::plot(x0t, y0t, "k-o", x0Ht, y0Ht, "g-o");
+    plt::plot({ar[0]}, {ar[0] * ar[0]}, "r*");
+    plt::subplot(2, 1, 2);
+    plt::plot(x1t, y1t, "k-", x1Ht, y1Ht, "g-");
+    plt::plot({ar[1]}, {ar[1] * ar[0]}, "r*");
+    plt::show();
   }
   LBFGSBInputs<T> params;
 };
